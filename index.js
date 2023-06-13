938a2c1794a27da4e411e963b0ad0a7b335c269b
unknown <ghimiremilan.gn@gmail.com>
2023-06-13 12:51:33 +0545

    java

---
+++ index.js
@@ -0,0 +1,17 @@
+//js operator
+
+//assignment operator
+let x='10';
+const y=10;
+
+//arthematic operation
+console.log(x+y);
+console.log(y-x);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+//comparison operator . It returens in bool
+console.log(x==y); //looks only value 
+console.log(x===y) // looks both value and its type 
+
