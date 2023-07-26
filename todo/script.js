9d45fa9bb8558951f40b287c655d907ae790ef61
unknown <ghimiremilan.gn@gmail.com>
2023-07-27 00:43:36 +0545

    todo started

---
+++ todo/script.js
@@ -0,0 +1,7 @@
+const todoinput=document.querySelector("#todo-input");
+const todoform=document.querySelector("#todo-form");
+const todolist=document.querySelector("#todo-list");
+
+todoform.addEventListener('submit',(event)=>{
+    event.preventDefault();
+})
