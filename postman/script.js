0f799033703f73a045accc7da0e8f9e78fb1d357
unknown <ghimiremilan.gn@gmail.com>
2023-07-26 23:50:53 +0545

    todo started

--- postman/script.js
+++ postman/script.js
@@ -1,19 +1,67 @@
 
-const data={
-    productname:"new iphone 15"
-}
+// const data={
+//     productname:"new iphone 15"
+// }
 
 
 
 
-const postdata= async (url , postdata) =>{
-    const response=await fetch(url,{
-        headers:{
-            'content-type':"application/json"},
-            method:"POST",
-            body:json.stringify(postdata)});
-            const data= await response.json();
-            console.log(data);
-        }
+// const postdata= async (url , postdata) =>{
+//     const response=await fetch(url,{
+//         headers:{
+//             'content-type':"application/json"},
+//             method:"POST",
+//             body:json.stringify(postdata)});
+//             const data= await response.json();
+//             console.log(data);
+//         }
     
-postdata ("https://jsonplaceholder.typicode.com/todos",data)
+// postdata ("https://jsonplaceholder.typicode.com/todos",data)
+
+
+
+
+
+
+const data={
+    productname:"iphone14"
+}
+const getdata=async (URL , datas) =>{
+    const response=await fetch(URL,{
+        headers:{
+        'content-type':'webapplication/JSON',
+        },
+        method:"POST",
+        body:JSON.stringify(datas)
+
+    });
+    const data=await response.json();
+    console.log(data);
+
+}
+getdata("https://dummyjson.com/products/add",data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
